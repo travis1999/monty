8fe50b64614bd9bb1feb683958aabd91bf056e51
travis1999 <trevoroguna@gmail.com>
2021-05-11 11:54:19 +0300

    initial commit

---
+++ monty.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * @argv: number of arguments
+ * @argc: char * array of arguments
+ * Return: Always (0) success
+ */
+
+int main(int argv, char **argc)
+{
+	return (0);
+}
