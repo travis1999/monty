d15547a38d46d99836a9d31aa51e56e0a796756d
travis1999 <trevoroguna@gmail.com>
2021-05-13 15:55:32 +0300

    Added readfile and simple parsing

--- monty.c
+++ monty.c
@@ -1,4 +1,7 @@
+#include "utils.h"
 #include <stdio.h>
+#include "list.h"
+
 
 /**
  * main - entry point of the program
@@ -9,10 +12,47 @@
 
 int main(int argv, char **argc)
 {
+	stack_t *program_stack = malloc(sizeof(program_stack)); 
+	FILE *fd;
+	char buffer[1024];
+	char ch = 'a';
+	size_t idx = 0;
+	size_t line_number = 0;
+
+	char *op;
+	char *value;
+
 	if (argv != 2)
 	{
-		printf("USAGE: monty file");
-		exit(1);
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
 	}
+
+	fd = open_file(argc[1]);
+
+	while (ch != EOF)
+	{
+		ch = fgetc(fd);
+		if (ch == '\n')
+		{
+			buffer[idx] = '\0';
+			idx = 0;
+			
+			line_number++;
+
+			op = strtok(buffer, " ");
+			value = strtok(NULL, " ");
+
+			printf("Line: %ld, {%s, %s}\n", line_number, op, value);
+			continue;
+		}
+
+		buffer[idx] = ch;
+		idx++;
+
+	}
+	
+	fclose(fd);
+	free(program_stack);
 	return (0);
 }
